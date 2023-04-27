5716f28f3037f83ecd34a4fd6b92fd90eb7be7e1
Muhammad Fikri <fikrimuhammad2016@gmail.com>
2023-04-27 13:18:13 +0700

    feature(slicing): - done slicing page home - navbar active by route - darkmode switcher - animate first open

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,17 @@
-import '@/styles/globals.css'
-import type { AppProps } from 'next/app'
+import '@/styles/globals.css';
+import type { AppProps } from 'next/app';
+import Head from 'next/head';
+import { ThemeProvider } from 'next-themes';
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <Head>
+        <title>Personal Website</title>
+      </Head>
+      <ThemeProvider attribute="class">
+        <Component {...pageProps} />
+      </ThemeProvider>
+    </>
+  );
 }
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,118 +1,146 @@
-import Image from 'next/image'
-import { Inter } from 'next/font/google'
+import Image from 'next/image';
+import { Poppins } from 'next/font/google';
+import { useSpring, animated } from '@react-spring/web';
+import { useState } from 'react';
+import { useTheme } from 'next-themes';
+import { useRouter } from 'next/router';
 
-const inter = Inter({ subsets: ['latin'] })
+const poppins = Poppins({
+  subsets: ['latin', 'devanagari', 'latin-ext'],
+  weight: '400',
+});
+
+function Navbar() {
+  const springs = useSpring({
+    from: { y: -50 },
+    to: { y: 5 },
+  });
+  const [menu, setMenu] = useState([
+    { label: 'Home', active: true },
+    { label: 'Project', active: false },
+    { label: 'Tools', active: false },
+  ]);
+  const [isDarkMode, setIsDarkMode] = useState(false);
+  const { theme, setTheme } = useTheme();
+  const router = useRouter();
+
+  function whenMenuClicked(to: string) {
+    router.push(`#${to}`);
+    const _menu = menu.map((val) => {
+      return val.label === to
+        ? { ...val, active: true }
+        : { ...val, active: false };
+    });
+
+    setMenu(_menu);
+  }
+
+  function toogleMode() {
+    setIsDarkMode(!isDarkMode);
+    theme == 'dark' ? setTheme('light') : setTheme('dark');
+  }
+  return (
+    <animated.div
+      style={{ ...springs }}
+      className={'flex justify-center gap-x-10 lg:gap-x-5 sticky top-5'}
+    >
+      <div className="flex justify-center gap-x-2 item-center p-1 px-2 bg-white bg-transparent opacity-90  dark:bg-slate-300 rounded-full">
+        {menu.map((val) => (
+          <button
+            key={val.label}
+            className={`p-3 text-sm tracking-wide text-center rounded-full ${
+              val.active && 'bg-slate-100 '
+            } hover:bg-slate-200`}
+            onClick={() => whenMenuClicked(val.label)}
+          >
+            {val.label}
+          </button>
+        ))}
+      </div>
+      <button
+        className="bg-white dark:bg-slate-300 hover:bg-slate-200 dark:hover:bg-slate-100 p-3 text-sm tracking-wide text-center rounded-full animate-bounce"
+        onClick={() => toogleMode()}
+      >
+        <Image
+          src={`/svg/${isDarkMode ? 'light' : 'dark'}-mode.svg`}
+          alt="Switcher"
+          width={32}
+          height={32}
+        />
+      </button>
+    </animated.div>
+  );
+}
 
 export default function Home() {
+  const springs = useSpring({
+    from: { y: 80 },
+    to: { y: 0 },
+  });
   return (
-    <main
-      className={`flex min-h-screen flex-col items-center justify-between p-24 ${inter.className}`}
-    >
-      <div className="z-10 w-full max-w-5xl items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">pages/index.tsx</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{' '}
+    <div className={`min-h-screen ${poppins.className}`}>
+      <Navbar />
+      <animated.main
+        style={{ ...springs }}
+        className={`flex flex-col items-baseline md:items-center p-20 px-5 md:p-24 md:px-0`}
+      >
+        <div className="max-w-xl mb-8">
+          <Image
+            className="bg-white dark:bg-slate-300 rounded-full mb-8"
+            src="/svg/profile.svg"
+            alt="Profile Pic"
+            width={100}
+            height={100}
+          />
+          <h1 className="text-4xl md:text-5xl leading-snug mb-8 font-semibold dark:text-slate-200">
+            Software Engineer
+          </h1>
+          <p className="text-lg md:text-xl text-gray-700 dark:text-slate-200 leading-relaxed font-thin">
+            Hi, I&apos;m Fikri. Software Engineer at{' '}
+            <a className="underline" href="https://bri.co.id" target="_blank">
+              BRI
+            </a>{' '}
+            also Student at{' '}
+            <a
+              className="underline"
+              href="https://nurulfikri.ac.id"
+              target="_blank"
+            >
+              STT Nurul Fikri.
+            </a>{' '}
+            Always Ready for New Opportunities and Challenges.{' '}
+          </p>
+        </div>
+        <div className="flex flex-row gap-x-4">
+          <a href="https://instagram.com/fikrimuha__" target="_blank">
             <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
+              src="/svg/ig.svg"
+              className="hover:bg-slate-500 dark:hover:bg-slate-200 hover:rounded-full"
+              alt="Instagram"
+              width={30}
+              height={30}
+            />
+          </a>
+          <a href="https://www.linkedin.com/in/muhammad-fikri-977a50192/" target="_blank">
+            <Image
+              src="/svg/linkedin.svg"
+              className="hover:bg-slate-500 dark:hover:bg-slate-200 hover:rounded-full"
+              alt="Linkedin"
+              width={30}
+              height={30}
+            />
+          </a>
+          <a href="https://github.com/fixdev" target="_blank">
+            <Image
+              src="/svg/gh.svg"
+              className="hover:bg-slate-500 dark:hover:bg-slate-200 hover:rounded-full"
+              alt="Github"
+              width={30}
+              height={30}
             />
           </a>
         </div>
-      </div>
-
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700/10 after:dark:from-sky-900 after:dark:via-[#0141ff]/40 before:lg:h-[360px]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
-
-      <div className="mb-32 grid text-center lg:mb-0 lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Docs{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Learn{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Templates{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Discover and deploy boilerplate example Next.js&nbsp;projects.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Deploy{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
-    </main>
-  )
+      </animated.main>
+    </div>
+  );
 }
